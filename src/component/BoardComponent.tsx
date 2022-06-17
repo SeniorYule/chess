81deff5685ee1c8e1c8c3de501252dab07341a80
SeniorYule <webmasterilyas@gmail.com>
2022-06-18 01:13:08 +0300

    Show Board

--- src/component/BoardComponent.tsx
+++ src/component/BoardComponent.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { FC } from "react";
+import {Board} from '../models/Board'
+import CellComponent from "./CellComponent";
+interface BoardProps{
+    board: Board,
+    setBoard : (board : Board) => void
+}
 
-const BoardComponent = () =>{
+const BoardComponent: FC<BoardProps> = ({board, setBoard}) =>{
     return (
         <div
             className="board"
         >
+            {board.cells.map((row , index) => 
+            <React.Fragment key={index}>
+                {row.map(cell => 
+                <CellComponent
+                
+                />
+                )}
+            </React.Fragment>
             
+            )}
+                
         </div>
     )
 }
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,3 +1,25 @@
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
 export class Cell{
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    availaple: boolean; /// Можешь ли переместиться
+    id : number; /// Для реакт ключей
     
+    constructor(board: Board, x: number, y:number, color: Colors, figure: Figure | null){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure =figure;
+        this.board = board;
+        this.availaple = false;
+        this.id = Math.random()
+
+
+    }
 }
