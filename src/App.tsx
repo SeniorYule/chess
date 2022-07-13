c282894d79926e8affb4af094f91886e530b6a76
SeniorYule <webmasterilyas@gmail.com>
2022-07-13 19:02:03 +0300

    peredacha hoda

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import BoardComponent from './component/BoardComponent';
 import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Player } from './models/Players';
 
 function App() {
   const [board, setBoard] = useState( new Board)
-
+  const [whitePlayer , setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer , setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const[currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
   useEffect(() => {
     restart()
+    setCurrentPlayer(whitePlayer)
   } , [])
 
   function restart(){
@@ -17,11 +22,17 @@ function App() {
     setBoard(newBoard)
   }
 
+  function swapPlayer(){
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+
   return (
     <div className="App">
       <BoardComponent
         board={board}
         setBoard ={setBoard}
+        currentPlayer = {currentPlayer}
+        swapPlayer = {swapPlayer}
       />
     </div>
   );
